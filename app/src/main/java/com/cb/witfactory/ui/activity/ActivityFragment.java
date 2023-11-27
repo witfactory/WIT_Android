af084577d49692f40ff86328e2e17a78c4c55f7f
Camilo Soto <camilo.soto@coxti.com>
2023-11-26 19:08:07 -0500

    Type: fix Desc: fixed devices list in activity fragment and add horizontal calendar

--- app/src/main/java/com/cb/witfactory/ui/activity/ActivityFragment.java
+++ app/src/main/java/com/cb/witfactory/ui/activity/ActivityFragment.java
@@ -1,6 +1,6 @@
 package com.cb.witfactory.ui.activity;
 
-import static org.chromium.base.ContextUtils.getApplicationContext;
+import static android.widget.Toast.LENGTH_SHORT;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +12,8 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,13 +22,13 @@ import androidx.lifecycle.ViewModelProvider;
 import com.cb.witfactory.R;
 import com.cb.witfactory.adapter.DeviceAdapter;
 import com.cb.witfactory.adapter.ListValueDeviceAdapter;
-import com.cb.witfactory.data.classModel.MyDividerItemDecoration;
 import com.cb.witfactory.data.retrofit.device.DeviceResponse;
 import com.cb.witfactory.data.retrofit.device.GetDeviceResponse;
 import com.cb.witfactory.data.retrofit.events.Metric;
 import com.cb.witfactory.databinding.FragmentActivityBinding;
 import com.cb.witfactory.model.Callfun;
 import com.cb.witfactory.ui.device.DeviceViewModel;
+import com.shrikanthravi.collapsiblecalendarview.data.Day;
 import com.shrikanthravi.collapsiblecalendarview.widget.CollapsibleCalendar;
 
 import java.text.SimpleDateFormat;
@@ -36,6 +38,9 @@ import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
+import in.akshit.horizontalcalendar.HorizontalCalendarView;
+import in.akshit.horizontalcalendar.Tools;
+
 public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAdapterListener, ListValueDeviceAdapter.ValueDeviceAdapterListener, Callfun {
 
     private FragmentActivityBinding binding;
@@ -44,6 +49,7 @@ public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAd
     private List<DeviceResponse> deviceList;  // Cambié ArrayList a List
     private AutoCompleteTextView autoCompleteTextViewDevice;
     private GetDeviceResponse getDeviceResponse;
+    private ArrayAdapter<String> adapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ActivityViewModel loginViewModel =
@@ -51,21 +57,46 @@ public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAd
 
         binding = FragmentActivityBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        autoCompleteTextViewDevice = root.findViewById(R.id.txt_country);
+
+        autoCompleteTextViewDevice = root.findViewById(R.id.txt_autocomplete);
         deviceViewModel = new ViewModelProvider(this).get(DeviceViewModel.class);
         deviceViewModel.setListener(ActivityFragment.this);
         deviceViewModel.getDataDevice("c8174124-b6b3-4a35-8457-429a9b947ea3", "S");
+
+        // Inicializa el adaptador una vez
+        adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line);
+
         getDeviceResponse = new GetDeviceResponse((ArrayList<DeviceResponse>) deviceList, 0, 0);
-        root = inflater.inflate(R.layout.fragment_activity, container, false);
+
         ImageButton btnCalendar = root.findViewById(R.id.btnCalendar);
         CollapsibleCalendar collapsibleCalendar = root.findViewById(R.id.collapsibleCalendar);
+        // Configurar el TextView con la fecha actual
 
-        // Configura un oyente para eventos de selección de fecha
+        // Configurar el HorizontalCalendar
+        HorizontalCalendarView calendarView = root.findViewById(R.id.horizontalCalendar);
+
+        Calendar starttime = Calendar.getInstance();
+        starttime.add(Calendar.MONTH, -6);
+
+        Calendar endtime = Calendar.getInstance();
+        endtime.add(Calendar.MONTH, 6);
+
+        ArrayList datesToBeColored = new ArrayList();
+        datesToBeColored.add(Tools.getFormattedDateToday());
+        calendarView.setUpCalendar(starttime.getTimeInMillis(),
+            endtime.getTimeInMillis(),
+            datesToBeColored,
+            new HorizontalCalendarView.OnCalendarListener() {
+                @Override
+                public void onDateSelected(String date) {
+                    Toast.makeText(requireContext(), "Tu mensaje aquí", Toast.LENGTH_SHORT).show();                    }
+            });
         binding.collapsibleCalendar.setCalendarListener(new CollapsibleCalendar.CalendarListener() {
 
             @Override
             public void onDaySelect() {
                 String selectedDate = binding.collapsibleCalendar.getSelectedDay().toString();
+                Log.d("TAG", "onDaySelect: "+ selectedDate);
             }
 
             @Override
@@ -90,8 +121,10 @@ public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAd
             public void onClick(View v) {
                 if (collapsibleCalendar.getVisibility() == View.VISIBLE) {
                     collapsibleCalendar.setVisibility(View.GONE);
+                    calendarView.setVisibility(View.VISIBLE);
                 } else {
                     collapsibleCalendar.setVisibility(View.VISIBLE);
+                    calendarView.setVisibility(View.GONE);
                 }
             }
         });
@@ -132,18 +165,16 @@ public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAd
 
             if (deviceList != null) {
                 List<String> deviceNames = getNameDevices(deviceList);
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_dropdown_item_1line, deviceNames);
+                adapter.clear();
+                adapter.addAll(deviceNames);
                 autoCompleteTextViewDevice.setAdapter(adapter);
 
-                autoCompleteTextViewDevice.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        String selectedDevice = (String) parent.getItemAtPosition(position);
-                        selectDevice(selectedDevice);
-                        // Llama a la función correspondiente al dispositivo seleccionado
-                        DeviceResponse deviceSelect = getDeviceResponse.getDeviceByName(deviceList, selectedDevice);
-                        selectDevice(deviceSelect.getDevice_id());
-                    }
+                autoCompleteTextViewDevice.setOnItemClickListener((parent, view, position, id) -> {
+                    String selectedDevice = (String) parent.getItemAtPosition(position);
+                    selectDevice(selectedDevice);
+                    // Llama a la función correspondiente al dispositivo seleccionado
+                    DeviceResponse deviceSelect = getDeviceResponse.getDeviceByName(deviceList, selectedDevice);
+                    selectDevice(deviceSelect.getDevice_id());
                 });
             }
         }
@@ -165,6 +196,12 @@ public class ActivityFragment extends Fragment implements DeviceAdapter.DeviceAd
         String deviceId = deviceList.get(0).getDevice_id();
         deviceViewModel.getMetrics(deviceId, fechaFormateada, fechaActualFormateada);
     }
-
-
+    public DeviceResponse getDeviceByName(List<DeviceResponse> devices, String deviceName) {
+        for (DeviceResponse device : devices) {
+            if (device.getDevice_name().equals(deviceName)) {
+                return device;
+            }
+        }
+        return null;  // Retorna null si no se encuentra el dispositivo con el nombre dado
+    }
 }
