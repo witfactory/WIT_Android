1cfd188dcae48925c4d2d7aca85143e9a6b6c3af
tapia2390 <tapia2390@gmail.com>
2023-12-02 00:07:07 -0500

    configuracion de la clase ESPDevice donde se hace la escritura ala hadware valdiado con mauricio

--- app/src/main/java/com/cb/witfactory/ui/connectDispositive/ConnectDispositiveViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.cb.witfactory.ui.connectDispositive;
-
-import androidx.lifecycle.ViewModel;
-
-public class ConnectDispositiveViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
